9b59ea375b8aacbf6bd4ae0be09801b60ac3bb06
Varvara Shinkarenko <shinkarenko.varvara@gmail.com>
2017-03-28 16:31:51 +0100

    Added 'play' functionality to the game, that changes turns. Tests passing.

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -8,11 +8,21 @@ describe("Game", function(){
   beforeEach(function(){
     playerOne = new Player('Varvara', 'X');
     playerTwo = new Player('Arthur', '0');
+    game = new Game(playerOne, playerTwo, board = new Board());
   });
 
   it("it assigns playerOne to play first at the start", function(){
-    game = new Game(playerOne, playerTwo, board = new Board());
     expect(game.turn).toEqual('X');
   });
+
+  it("lets player to play", function(){
+    expect(game.play).toBeDefined();
+  });
+
+  it("after one player has played, the turn is changed", function(){
+    game.play();
+    expect(game.turn).toEqual('0')
+  });
+
 
 });
--- src/game.js
+++ src/game.js
@@ -1,13 +1,25 @@
-//understands the processes in the game - whose turn is it, if anybody wins, and when it all ends
+//understands the processes in the game, how to play - whose turn is it, if anybody wins, and when it all ends
 
 (function(exports){
   function Game(playerOne, playerTwo, board = new Board()){
     this.playerOne = playerOne;
     this.playerTwo = playerTwo;
     this.board = board;
-    // this.ruleBook = ruleBook;
     this.turn = playerOne.role;
+
+    this.play = function () {
+      this.board.claimField();
+      this.changeTurn();
+      return this.turn
+    };
   }
+
+
+  Game.prototype.changeTurn =  function (){
+    console.log(this);
+    this.turn === this.playerOne.role ? this.turn = this.playerTwo.role : this.turn = this.playerOne.role
+  };
+
 
   exports.Game = Game;
 
