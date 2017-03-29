c140014b17fe3466b01bfff60b8617c856e1b1e4
Varvara Shinkarenko <shinkarenko.varvara@gmail.com>
2017-03-29 15:57:31 +0100

    Added new tests with rulebook  validating the game

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -4,31 +4,36 @@ describe("Game", function(){
   var board;
   var playerOne;
   var playerTwo;
+  var ruleBook;
+  // layerOne, board = new Board());}).toThrow("There should be at least two players");  // });
 
-  beforeEach(function(){
-    playerOne = new Player('Varvara', 'X');
-    playerTwo = new Player('Arthur', '0');
-    game = new Game(playerOne, playerTwo, board = new Board());
+    beforeEach(function(){
+      playerOne = new Player('Varvara', 'X');
+      playerTwo = new Player('Arthur', '0');
+      game = new Game(playerOne, playerTwo, board = new Board(), rulebook = new Rulebook());
+    });
+
+    it("it assigns playerOne to play first at the start", function(){
+      expect(game.turn).toEqual('X');
+    });
+
+    it("lets player to play", function(){
+      expect(game.play(1, 2)).toBeTruthy();
+    });
+
+    it("after one player has played, the turn is changed", function(){
+      game.play(1, 2);
+      expect(game.turn).toEqual('0');
+    });
+
+    it("should throw error massage if the field is outside of the board", function(){
+      expect(function() {game.play(3, 2);}).toThrowError("Field is outside the board");
+    });
+
+
+    it("should throw error massage if the field is taken", function(){
+      game.play(1, 2);
+      expect(function() {game.play(1, 2);}).toThrowError("Field is taken, try another one");
+    });
+
   });
-
-  it("it assigns playerOne to play first at the start", function(){
-    expect(game.turn).toEqual('X');
-  });
-
-  it("lets player to play", function(){
-    expect(game.play(1, 2)).toBeTruthy();
-  });
-
-  it("after one player has played, the turn is changed", function(){
-    game.play(1, 2);
-    expect(game.turn).toEqual('0');
-  });
-
-  it("should throw error massage if the field is taken", function(){
-    game.play(1, 2);
-    expect(function() {game.play(1, 2);}).toThrowError("Field is taken, try another one");
-    // expect(game.play(1, 2)).toThrow(new Error("Field is taken, try another one"));
-  });
-
-
-});
--- src/game.js
+++ src/game.js
@@ -1,17 +1,19 @@
 //understands the processes in the game, how to play - whose turn is it, if anybody wins, and when it all ends
 
 (function(exports){
-  function Game(playerOne, playerTwo, board = new Board()){
+  function Game(playerOne, playerTwo, board = new Board(), ruleBook = new Rulebook()){
     this.playerOne = playerOne;
     this.playerTwo = playerTwo;
     this.board = board;
+    this.ruleBook = ruleBook;
     this.turn = playerOne.role;
 
     this.play = function (row, column) {
+      if (this.ruleBook.isClaimedIncorrectly(row, column)) throw Error('Field is outside the board');
       if (this.board.isFieldTaken(row, column)) throw Error('Field is taken, try another one');
-      this.board.claimField(row, column, role = this.turn);
+      this.board.claimField(row, column, role = this.turn, ruleBook);
       this.changeTurn();
-      return "Now is the turn of" + this.turn;
+      return "Now is the turn of " + this.turn;
     };
   }
 
